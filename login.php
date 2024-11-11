1c8e7cfa66ae5edb6c201c5228d51ba82ca0caf4
Aryan Kedare <arykedare@gmail.com>
2024-11-11 09:34:25 +0000

    login: Change login logic for admin panel

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres password=123") or die("Connection Failed");
-	$uname = $_POST['email'];
+		//$uname = $_POST['email'];
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         $uname = $_POST['uname'];
         $pwd = $_POST['pwd'];
@@ -16,11 +16,12 @@ $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres passwo
 		}elseif($type==="Company"){
 				$GLOBALS['sql']="SELECT pwd from companys where email='".$_POST['uname']."';";
 				$result=pg_query($GLOBALS['conn'],$sql);
-		}elseif($type==="Admin"&&$pwd==="aryan"&&$uname==="admin@tpc.in"){
-		header('Location: admin_dash.php');
+		}elseif($type==="Admin"){
+			$GLOBALS['sql']="SELECT pwd from admins where email='".$_POST['uname']."';";
+			$result=pg_query($GLOBALS['conn'],$sql);
 		}else{
 			
-			echo "<SCRIPT type='text/javascript'> //not showing me this
+			echo "<SCRIPT type='text/javascript'>
 								alert('Wrong username or password to continue as admin');
 								window.location.replace(\"index.html\");
 							</SCRIPT>";
@@ -28,42 +29,49 @@ $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres passwo
 		
         
        // $result = $GLOBALS['conn']->query($sql);
-        if($type==="Student"||$type==="Company"){
+        if($type==="Student"||$type==="Company"|$type==="Admin"){
         if(pg_num_rows($result) > 0){
             $row=pg_fetch_assoc($result);
             if($row["pwd"]===$pwd){
 				if($GLOBALS['type']==="Student"){
 					$_SESSION['email']=$GLOBALS['uname'];
 					header('Location: student_dash.php');
-					echo "<SCRIPT type='text/javascript'> //not showing me this
+					echo "<SCRIPT type='text/javascript'>
 								window.location.replace(\"student_dash.php\");
 							</SCRIPT>";
 				}elseif($GLOBALS['type']==="Company"){
 					 $_SESSION['email']=$GLOBALS['uname'];
 					//header('Location: company_dash.php');
-					echo "<SCRIPT type='text/javascript'> //not showing me this
-								
+					echo "<SCRIPT type='text/javascript'> 
+
 								window.location.replace(\"company_dash.php\");
 							</SCRIPT>";
 				}
-				
-                //echo "Login successful <BR>";
+				elseif($GLOBALS['type']==="Admin"){
+					$_SESSION['email']=$GLOBALS['uname'];
+					echo "login verified";
+					header('Location: admin_dash.php');
+				    echo "<SCRIPT type='text/javascript'> 
+							   window.location.replace(\"admin_dash.php\");
+						   </SCRIPT>";
+			   }	
+                echo "Login successful <BR>";
             }else{
-                echo "<SCRIPT type='text/javascript'> //not showing me this
-								alert('Wrong password!');
+                echo "<SCRIPT type='text/javascript'> 
+								alert('Invalid username or password');
 								window.location.replace(\"index.html\");
 							</SCRIPT>";
             }
 
         } 
 		else{
-           echo "<SCRIPT type='text/javascript'> //not showing me this
+           echo "<SCRIPT type='text/javascript'>
 								alert('User Does not exist!');
 								window.location.replace(\"index.html\");
 							</SCRIPT>";
         }
-		}
-        echo $_SESSION['email'] . "<BR>";
+			}
+	  echo $_SESSION['email'] . "<BR>";
         //echo $pwd . "<BR>";
     }
 		pg_close($conn);

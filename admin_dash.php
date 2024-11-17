26dfd1c0f6ff3a23c6abc1a94f2af16c21acc345
Aryan Kedare <arykedare@gmail.com>
2024-11-17 16:59:02 +0000

    admin_panel: fix broken psql queries and cleanup

--- admin_dash.php
+++ admin_dash.php
@@ -1,79 +1,60 @@
-<html>
-	<head>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="CSS/style1.css">
-		<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-        <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-        
-		<nav class="navbar navbar-fixed-top" id="top-nav">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#">Campus Recruitment System</a>
-				</div>
-				<ul id="list1" class="nav navbar-nav">
-					<li class="active"><a href="index.html">Home</a></li>
-		           
-				   <li class="active"><a href="delete_student_a.php">Delete Student</a></li>
-				   <li class="active"><a href="delete_company_a.php">Delete Company</a></li>
-				   <li class="active"><a href="index.html">Logout</a></li>
-				</ul>
-			</div>
-		</nav>
-		  
-		   
-		
-	</head>
-	
-	<body>
-	 <h2 align="center">ADMIN DASHBOARD</h2>
-		<div class=" container-fluid" id="dash">
-		  <div class="row">
-		    <div class="well col-sm-12" style="background-color: #AED4F1 ; height:auto; margin:10px;">
-		      
-			  
-			  <br>
-			  <br>
-			  <div>
+<?php
+session_start();
+$email = $_SESSION['email'] ?? '';
+$name = $_SESSION['name'] ?? '';
+$conn = pg_connect("host=localhost port=5432 dbname=project user=postgres password=123") or die("Connection Failed");
+?>
 
-			  <?php
-			  session_start();
-			  $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres password=123") or die("Connection Failed");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/style1.css">
+</head>
+<body>
+    <nav class="navbar navbar-fixed-top" id="top-nav">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Campus Recruitment System</a>
+            </div>
+            <ul id="list1" class="nav navbar-nav">
+                <li class="active"><a href="index.html">Home</a></li>
+                <li><a href="delete_student_a.php">Delete Student</a></li>
+                <li><a href="delete_company_a.php">Delete Company</a></li>
+                <li><a href="index.html">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
 
-			 echo "<style>
-                table, th, td {
-                   border: 2px solid black;
-                   border-collapse: collapse;
-				   }
-                th, td {
-                   padding: 5px;
-                    text-align: left;    
-                          }
-                 </style>";
-						  
-		 echo  " <form method=\"POST\" action=\"\">
-            <select type=\"text\" name=\"name\" placeholder=\"Select\">
-			<option label=\"cts\">SELECT TO CHECK</option>
-		   <option label=\"trs\">TOTAL REGISTERED STUDENTS</option>
-          <option label=\"tps\">TOTAL PLACED STUDENTS</option>
-	      <option label=\"trc\">TOTAL REGISTERED COMPANIES</option>
-		  <option label=\"trc\">STUDENTS PLACED IN BANGLORE</option>
-		  <option label=\"trc\">STUDENTS PLACED AS DEVELOPERS</option>
-		  <option label=\"trc\">TOTAL VACANCIES</option>
-       <input type=\"submit\"></input>
-        </form>" ;
-				  if(isset($_POST['name']) && $_POST['name']=="TOTAL REGISTERED STUDENTS") {
-                     
-               	 $sql = "SELECT * FROM students";
+    <div class="container">
+        <h2 class="text-center">ADMIN DASHBOARD</h2>
+        <div class="well" style="background-color: #AED4F1; margin-top: 20px;">
+            <form method="POST" action="">
+                <select name="query" class="form-control">
+                    <option value="">SELECT TO CHECK</option>
+                    <option value="registered_students">TOTAL REGISTERED STUDENTS</option>
+                    <option value="placed_students">TOTAL PLACED STUDENTS</option>
+                    <option value="registered_companies">TOTAL REGISTERED COMPANIES</option>
+                    <option value="students_bangalore">STUDENTS PLACED IN BANGALORE</option>
+                    <option value="students_developers">STUDENTS PLACED AS DEVELOPERS</option>
+                    <option value="total_vacancies">TOTAL VACANCIES</option>
+                </select>
+                <button type="submit" class="btn btn-primary mt-2">Submit</button>
+            </form>
+
+            <?php
+			  if(isset($_POST['query']) && $_POST['query']=="registered_students") {
+               	$sql = "SELECT * FROM students";
                 $result = pg_query($conn,$sql);
 				
                echo "<h4 align=\"center\">REGISTERED STUDENTS</h4>";
 			   echo "<br>";
              if (pg_num_rows($result) > 0) {
                  echo "<table class=\"table table-hover\"><tr><th>Name</th><th>Email</th><th>Contact No.</th><th>D.O.B.</th><th>Degree</th><th>Branch</th><th>Year of Passing</th><th>C.P.I.</th><th>12th Percentage</th><th>10th Percentage</th></tr>";
-     
                while($row = pg_fetch_assoc($result)) {
                 echo "<tr><td>".$row["name"]."</td><td>".$row["email"]."</td><td>".$row["phone"]."</td><td>".$row["dob"]."</td><td>".$row["degree"]."</td><td> ".$row["branch"]."</td><td>".$row["year"]."</td><td>".$row["cpi"]."</td><td>".$row["twp"]."</td><td>".$row["tenp"]."</td></tr>";
                     }
@@ -83,7 +64,7 @@
                }
 				  }
 				  
-			  if(isset($_POST['name']) && $_POST['name']=="TOTAL VACANCIES") {
+				  if(isset($_POST['query']) && $_POST['query']=="total_vacancies") {
                      
                	 $sql = "SELECT * FROM vacancy";
                 $result = pg_query($conn,$sql);
@@ -103,7 +84,7 @@
 				  }
 				  
 				  
-			if(isset($_POST['name']) && $_POST['name']=="TOTAL REGISTERED COMPANIES") {
+				  if(isset($_POST['query']) && $_POST['query']=="registered_companies") {
                      
                	 $sql = "SELECT * FROM companys";
                 $result = pg_query($conn,$sql);
@@ -120,7 +101,7 @@
                }
 				  }
 				  
-		 if(isset($_POST['name']) && $_POST['name']=="TOTAL PLACED STUDENTS") {
+				  if(isset($_POST['query']) && $_POST['query']=="placed_students") {
                      
                	 $sql = "SELECT * FROM students as s,applications as a,vacancy as v where s.email=a.s_mail and a.job_id=v.job_id and a.status='1' ";
                 $result = pg_query($conn,$sql);
@@ -139,7 +120,7 @@
                }
 				  }
 				  
-		  if(isset($_POST['name']) && $_POST['name']=="STUDENTS PLACED IN BANGLORE") {
+				  if(isset($_POST['query']) && $_POST['query']=="students_bangalore") {
                      
                	 $sql = "SELECT * FROM students as S,applications as A,vacancy as V where S.email=A.s_mail and A.job_id=V.job_id and A.status='1' and V.location='Banglore'";
                 $result = pg_query($conn,$sql);
@@ -158,7 +139,7 @@
                }
 				  }
 		
-				  if(isset($_POST['name']) && $_POST['name']=="STUDENTS PLACED AS DEVELOPERS") {
+				  if(isset($_POST['query']) && $_POST['query']=="students_developers") {
                      
                	 $sql = "SELECT * FROM students as S,applications as A,vacancy as V where S.email=A.s_mail and A.job_id=V.job_id and A.status='1' and V.job_title='Developer'";
                 $result = pg_query($conn,$sql);
@@ -177,51 +158,14 @@
                }
 				  }
            pg_close($conn);
-          ?>
+          ?>        </div>
+    </div>
 
+    <footer>
+        <!-- Footer content -->
+    </footer>
 
-					
-					
-			</div>
-		  </div>
-		</div> 
- </div>
-
-	</body>
-
-<footer>
-		<nav class="navbar navbad-default" id="bottom-nav">
-			<div class="container-fluid">
-				<div id="col1" >
-					<ul id="blist1">
-						<li><a href='#'>About Us</a></li>
-						<li><a href='#'>FAQs</a></li>
-						<li><a href='#'>Contact Us</a></li>
-					</ul>
-				</div>
-				
-				<div id="col2" >
-					<ul id="blist1">
-						<li><a href='#'>Privacy Policy</a></li>
-						<li><a href='#'>Legal</a></li>
-						<li><a href='#'>Work With Us</a></li>
-					</ul>
-				</div>
-				
-				<div id="col3" class=" container-fluid">
-				
-					<ul id="blist3" >
-						<li><i class="fa fa-facebook fa-2x" ><a href='#'></a></i></li>
-						<li><i class="fa fa-twitter fa-2x"><a href='#'></a></i></li>
-						<li><i class="fa fa-instagram fa-2x"><a href='#'></a></i></li>
-						<li><i class="fa fa-linkedin fa-2x"><a href='#'></a></i></li>
-					</ul>
-					<ul>
-					<!--<p id="lic">site design / logo (c) Company_Name Inc.<br> licensed under Company_Name inc. </p>-->
-					<li id="blist4">site design / logo (c) Company_Name Inc.<br> licensed under Company_Name inc.</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
 </html>

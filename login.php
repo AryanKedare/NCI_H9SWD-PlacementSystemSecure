0ada1ea0b767d0e7df6de9732d90bf5948c3b409
Aryan Kedare <arykedare@gmail.com>
2024-11-17 14:26:05 +0000

    login: add basic SQL injection projection

--- login.php
+++ login.php
@@ -1,78 +1,63 @@
 <?php
-	session_start();
+session_start();
 $conn = pg_connect("host=localhost port=5432 dbname=project user=postgres password=123") or die("Connection Failed");
-		//$uname = $_POST['email'];
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $uname = $_POST['uname'];
-        $pwd = $_POST['pwd'];
-		$type = $_POST['type'];
-		$sql="";
-		function phpAlert($msg) {
-    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
-		}
-		if($type==="Student"){
-				$GLOBALS['sql']="SELECT pwd from students where email='".$_POST['uname']."';";
-				$result=pg_query($GLOBALS['conn'],$sql);
-		}elseif($type==="Company"){
-				$GLOBALS['sql']="SELECT pwd from companys where email='".$_POST['uname']."';";
-				$result=pg_query($GLOBALS['conn'],$sql);
-		}elseif($type==="Admin"){
-			$GLOBALS['sql']="SELECT pwd from admins where email='".$_POST['uname']."';";
-			$result=pg_query($GLOBALS['conn'],$sql);
-		}else{
-			
-			echo "<SCRIPT type='text/javascript'>
-								alert('Wrong username or password to continue as admin');
-								window.location.replace(\"index.html\");
-							</SCRIPT>";
-		}
-		
-        
-       // $result = $GLOBALS['conn']->query($sql);
-        if($type==="Student"||$type==="Company"|$type==="Admin"){
-        if(pg_num_rows($result) > 0){
-            $row=pg_fetch_assoc($result);
-            if($row["pwd"]===$pwd){
-				if($GLOBALS['type']==="Student"){
-					$_SESSION['email']=$GLOBALS['uname'];
-					header('Location: student_dash.php');
-					echo "<SCRIPT type='text/javascript'>
-								window.location.replace(\"student_dash.php\");
-							</SCRIPT>";
-				}elseif($GLOBALS['type']==="Company"){
-					 $_SESSION['email']=$GLOBALS['uname'];
-					//header('Location: company_dash.php');
-					echo "<SCRIPT type='text/javascript'> 
 
-								window.location.replace(\"company_dash.php\");
-							</SCRIPT>";
-				}
-				elseif($GLOBALS['type']==="Admin"){
-					$_SESSION['email']=$GLOBALS['uname'];
-					echo "login verified";
-					header('Location: admin_dash.php');
-				    echo "<SCRIPT type='text/javascript'> 
-							   window.location.replace(\"admin_dash.php\");
-						   </SCRIPT>";
-			   }	
-                echo "Login successful <BR>";
-            }else{
-                echo "<SCRIPT type='text/javascript'> 
-								alert('Invalid username or password');
-								window.location.replace(\"index.html\");
-							</SCRIPT>";
-            }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $uname = $_POST['uname'];
+    $pwd = $_POST['pwd'];
+    $type = $_POST['type'];
 
-        } 
-		else{
-           echo "<SCRIPT type='text/javascript'>
-								alert('User Does not exist!');
-								window.location.replace(\"index.html\");
-							</SCRIPT>";
-        }
-			}
-	  echo $_SESSION['email'] . "<BR>";
-        //echo $pwd . "<BR>";
+    function phpAlert($msg) {
+        echo '<script type="text/javascript">alert("' . $msg . '")</script>';
     }
-		pg_close($conn);
+
+    // Prepare the SQL query based on the user type
+    if ($type === "Student") {
+        $sql = "SELECT pwd FROM students WHERE email = $1";
+    } elseif ($type === "Company") {
+        $sql = "SELECT pwd FROM companys WHERE email = $1";
+    } elseif ($type === "Admin") {
+        $sql = "SELECT pwd FROM admins WHERE email = $1";
+    } else {
+        echo "<SCRIPT type='text/javascript'>
+                alert('Wrong username or password to continue as admin');
+                window.location.replace(\"index.html\");
+              </SCRIPT>";
+        exit();
+    }
+
+    // Prepare and execute the query using parameterized queries
+    $result = pg_prepare($conn, "login_query", $sql);
+    $result = pg_execute($conn, "login_query", array($uname));
+
+    if ($result && pg_num_rows($result) > 0) {
+        $row = pg_fetch_assoc($result);
+        if ($row["pwd"] === $pwd) {
+            // Set session variables based on user type
+            $_SESSION['email'] = $uname;
+            if ($type === "Student") {
+                header('Location: student_dash.php');
+                exit();
+            } elseif ($type === "Company") {
+                header('Location: company_dash.php');
+                exit();
+            } elseif ($type === "Admin") {
+                header('Location: admin_dash.php');
+                exit();
+            }
+        } else {
+            echo "<SCRIPT type='text/javascript'>
+                    alert('Invalid username or password');
+                    window.location.replace(\"index.html\");
+                  </SCRIPT>";
+        }
+    } else {
+        echo "<SCRIPT type='text/javascript'>
+                alert('User does not exist!');
+                window.location.replace(\"index.html\");
+              </SCRIPT>";
+    }
+}
+
+pg_close($conn);
 ?>
